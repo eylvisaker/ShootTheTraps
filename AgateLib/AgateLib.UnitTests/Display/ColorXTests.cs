94a706e4bb31a0cc1f88ce307b7590aa7d215977
Erik Ylvisaker <eylvisaker@gmail.com>
2018-10-24 05:38:57 -0700

    git subrepo pull AgateLib
    subrepo:
      subdir:   "AgateLib"
      merged:   "23030fa0"
    upstream:
      origin:   "https://github.com/eylvisaker/AgateLib"
      branch:   "master"
      commit:   "c68a5d51"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

---
+++ AgateLib/AgateLib.UnitTests/Display/ColorXTests.cs
@@ -0,0 +1,18 @@
+﻿using AgateLib.Display;
+using FluentAssertions;
+using Microsoft.Xna.Framework;
+using Xunit;
+
+namespace AgateLib.Tests.Display
+{
+    public class ColorXTests
+    {
+        [Fact]
+        public void ToArgb()
+        {
+            var clr = new Color(0x12, 0x34, 0x56, 0x78);
+
+            clr.ToArgb().Should().Be("78123456");
+        }
+    }
+}
